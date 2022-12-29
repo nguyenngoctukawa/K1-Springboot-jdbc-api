868f84c314aa1b2cfc2ccc92d6bed5c363cba980
tunguyen <tunguyenkawa@gmail.com>
2022-12-29 07:14:33 +0700

    C R U D => DB

--- src/main/java/com/example/nbs/springbasic/springboot_jdbc_api/SpringbootJdbcApiApplication.java
+++ src/main/java/com/example/nbs/springbasic/springboot_jdbc_api/SpringbootJdbcApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Date;
+
 @SpringBootApplication
 public class SpringbootJdbcApiApplication implements CommandLineRunner {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -15,6 +17,16 @@ public class SpringbootJdbcApiApplication implements CommandLineRunner {
 	PlayerDAO playerDao;
 	@Override
 	public void run(String... args) throws Exception {
+		logger.info("Inserting Player 4: {}", playerDao.insertPlayer(
+				new Player (4, "Thiem", "Austria", new Date(System.currentTimeMillis()), 17 ))
+		);
+
+		logger.info("Updating Player with Id 4: {}", playerDao.updatePlayer(
+				new Player(4, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17))
+		);
+
+		logger.info("Deleting Player with Id 2: {}", playerDao.deletePlayerById(2));
+
 		logger.info("All Players Data: {}", playerDao.getAllPlayers());
 
 		logger.info("Player with Id 3: {}", playerDao.getPlayerById(3));
