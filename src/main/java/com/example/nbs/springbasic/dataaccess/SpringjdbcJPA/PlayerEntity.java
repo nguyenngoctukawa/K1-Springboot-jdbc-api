5b178cad9fb6291d24821d1338bdd1293b3d345d
tunguyen <tunguyenkawa@gmail.com>
2022-12-30 07:22:27 +0700

    PlayerEntity: @Entity @Table() => application: jpa show sql (hibernate)

---
+++ src/main/java/com/example/nbs/springbasic/dataaccess/SpringjdbcJPA/PlayerEntity.java
@@ -0,0 +1,88 @@
+package com.example.nbs.springbasic.dataaccess.SpringjdbcJPA;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name="PlayerTable")
+public class PlayerEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String nationality;
+
+    private Date birthDate;
+    private int titles;
+
+    public PlayerEntity( ) {
+
+    }
+
+    public PlayerEntity(String name, String nationality, Date birthDate, int titles) {
+        super();
+        this.name = name;
+        this.nationality = nationality;
+        this.birthDate = birthDate;
+        this.titles = titles;
+    }
+
+    public PlayerEntity(int id, String name, String nationality, Date birthDate, int titles) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+        this.birthDate = birthDate;
+        this.titles = titles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getTitles() {
+        return titles;
+    }
+
+    public void setTitles(int titles) {
+        this.titles = titles;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPlayer [id= " + id + ", name= " + name + ", nationality= " + nationality + ", birthDate= " + birthDate
+                + ", titles= " + titles + "]";
+    }
+}
